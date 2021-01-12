dbbf1e3134c4d152191fe5cd5005352f22277bea
Volodymyr Lykhvar <vlykhvar@gmail.com>
2021-01-12 04:47:36 +0200

    added Storage.java according to requirements

---
+++ src/main/java/mate/hwdao/db/Storage.java
@@ -0,0 +1,14 @@
+package mate.hwdao.db;
+
+
+import java.util.ArrayList;
+import java.util.List;
+import mate.hwdao.model.Car;
+import mate.hwdao.model.Driver;
+import mate.hwdao.model.Manufacturer;
+
+public class Storage {
+    public static final List<Car> listCar = new ArrayList<>();
+    public static final List<Driver> listDriver = new ArrayList<>();
+    public static final List<Manufacturer> listManufacturer = new ArrayList<>();
+}
