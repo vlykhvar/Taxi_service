15e6ccf0c13864075b39a4d1fd6408385b45252b
Volodymyr Lykhvar <vlykhvar@gmail.com>
2021-01-12 04:49:23 +0200

    added ManufacturerDao according to requirements.

---
+++ src/main/java/mate/hwdao/dao/ManufacturerDao.java
@@ -0,0 +1,14 @@
+package mate.hwdao.dao;
+
+import java.util.List;
+import java.util.Optional;
+import mate.hwdao.model.Manufacturer;
+
+public interface ManufacturerDao {
+
+    public Manufacturer create(Manufacturer manufacturer);
+    public Optional<Manufacturer> get(Long id);
+    public List<Manufacturer> getAll();
+    public Manufacturer update(Manufacturer manufacturer);
+    public boolean delete(Long id);
+}
