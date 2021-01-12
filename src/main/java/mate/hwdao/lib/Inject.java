02d366bd49bd17eea30826ddf526e553f132ab99
Volodymyr Lykhvar <vlykhvar@gmail.com>
2021-01-12 04:52:10 +0200

    added annotations according to requirements

---
+++ src/main/java/mate/hwdao/lib/Inject.java
@@ -0,0 +1,9 @@
+package mate.hwdao.lib;
+
+
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+
+@Retention(RetentionPolicy.RUNTIME)
+public @interface Inject {
+}
