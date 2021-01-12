9f24960da12d7ba1efa8b64f64da2a463174103f
Volodymyr Lykhvar <vlykhvar@gmail.com>
2021-01-12 17:07:30 +0200

    deleted rudiment checked

--- src/main/java/mate/hwdao/Main.java
+++ src/main/java/mate/hwdao/Main.java
@@ -1,10 +1,7 @@
 package mate.hwdao;
 
-import mate.hwdao.db.Storage;
 import mate.hwdao.lib.Inject;
 import mate.hwdao.lib.Injector;
-import mate.hwdao.model.Car;
-import mate.hwdao.model.Driver;
 import mate.hwdao.model.Manufacturer;
 import mate.hwdao.service.ManufacturerService;
 import mate.hwdao.service.ManufacturerServiceImpl;
@@ -26,20 +23,11 @@ public class Main {
         System.out.println("How to work manufacturer service.create: " + manufacturerZaz.toString());
         System.out.println("How to work manufacturer service.getAll: " + manufacturerService.getAll().toString());
         System.out.println("How to work manufacturer service.getId: " + manufacturerService.get(manufacturerHonda.getId()).toString());
-        Car carVida = new Car("Vida", manufacturerZaz);
-        Driver driverSofia = new Driver("Sofia", "0001");
-        Storage.listDriver.add(driverSofia);
-        Driver driverBogdan = new Driver("Bogdan", "0002");
-        Storage.listDriver.add(driverBogdan);
-        Driver driverRoman = new Driver("Roman" , "0003");
-        Storage.listDriver.add(driverRoman);
         manufacturerService.update(manufacturerHonda).setCountry("Japan");
         System.out.println("After change country: " + manufacturerHonda.toString());
         manufacturerService.delete(manufacturerHonda.getId());
         System.out.println(manufacturerService.getAll().toString());
-        carVida.getDrivers().add(driverSofia);
-        carVida.getDrivers().add(driverBogdan);
-        carVida.getDrivers().add(driverRoman);
-        System.out.println(carVida.toString());
+
     }
+
 }
