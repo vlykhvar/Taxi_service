23e0fa484fb2a6ce56c6fd4410bbe45e76ce9c20
Volodymyr Lykhvar <vlykhvar@gmail.com>
2021-01-14 08:41:30 +0200

    deleted rudiment code/throws

--- src/main/java/mate/hwdao/Main.java
+++ src/main/java/mate/hwdao/Main.java
@@ -1,6 +1,5 @@
 package mate.hwdao;
 
-import java.sql.SQLException;
 import mate.hwdao.lib.Injector;
 import mate.hwdao.model.Manufacturer;
 import mate.hwdao.service.ManufacturerService;
@@ -10,7 +9,7 @@ public class Main {
 
     private static final Injector injector = Injector.getInstance(Main.class.getPackageName());
 
-    public static void main(String[] args) throws SQLException, InterruptedException {
+    public static void main(String[] args) {
         ConnectionUtil.setBase();
         System.out.println("All the characters and events depicted are fictitious. "
                 + "Any resemblance to a person living or dead is purely coincidental");
@@ -37,44 +36,6 @@ public class Main {
         System.out.println("How to work service.getAll after deleted: "
                 + manufacturerService.getAll().toString());
         ConnectionUtil.dropTable();
-        /*Driver driverSofia = new Driver("Sofia", "0001");
-        Driver driverBogdan = new Driver("Bogdan", "0002");
-        Driver driverRoman = new Driver("Roman", "0003");
-        Driver driverKsenia = new Driver("Ksenia", "0004");
-        Driver driverDmytro = new Driver("Dmytro", "0004");
-        DriverService driverService = (DriverService) injector.getInstance(DriverService.class);
-        driverService.create(driverSofia);
-        driverService.create(driverBogdan);
-        driverService.create(driverRoman);
-        driverService.create(driverKsenia);
-        driverService.create(driverDmytro);
-        System.out.println("Here we have: "
-                + driverService.getAll().toString());
-        Car carVida = new Car("Vida", manufacturerZaz);
-        Car carCivic = new Car("Civic", manufacturerHonda);
-        Car carFocus = new Car("Focus", manufacturerFord);
-        Car carC5 = new Car("C5", manufacturerCitroen);
-        CarService carService = (CarService) injector.getInstance(CarService.class);
-        carService.create(carVida);
-        carService.create(carCivic);
-        carService.create(carFocus);
-        carService.create(carC5);
-        System.out.println("We have park of auto: "
-                + carService.getAll().toString());
-        carService.addDriverToCar(driverSofia, carCivic);
-        carService.addDriverToCar(driverBogdan, carCivic);
-        carService.addDriverToCar(driverRoman, carCivic);
-        carService.addDriverToCar(driverKsenia, carCivic);
-        carService.addDriverToCar(driverDmytro, carCivic);
-        carService.addDriverToCar(driverBogdan, carFocus);
-        carService.addDriverToCar(driverBogdan, carC5);
-        carService.addDriverToCar(driverBogdan, carVida);
-        System.out.println("Driver Bogdan ride on "
-                + carService.getAllByDriver(driverBogdan.getId()).toString());
-        System.out.println("But Bogdan diced not to work on "
-                + carVida.toString());
-        carService.removeDriverFromCar(driverBogdan, carVida);
-        System.out.println("His new list of work car is "
-                + carService.getAllByDriver(driverBogdan.getId()).toString());*/
+       
     }
 }
