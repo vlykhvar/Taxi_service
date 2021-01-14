a0112e45886300f4b4e3732fc3f194fe0b586b18
Volodymyr Lykhvar <vlykhvar@gmail.com>
2021-01-14 06:13:52 +0200

    added ConnectionUtil.java. added method for connection to db, create and drop table

--- src/main/java/mate/hwdao/util/ConnectionUtil.java
+++ src/main/java/mate/hwdao/util/ConnectionUtil.java
@@ -2,13 +2,14 @@ package mate.hwdao.util;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.PreparedStatement;
 import java.sql.SQLException;
 import java.util.Properties;
 
 public class ConnectionUtil {
     private final static String LOGIN = "root";
     private final static String PASSWORD = "root";
-    private final static String URL = "jdbc:mysql://localhost:3306"
+    private final static String URL = "jdbc:mysql://localhost:3306/dao"
             + "?useUnicode=true&useJDBCCompliantTimezoneShift="
             + "true&useLegacyDatetimeCode=false&serverTimezone=UTC";
     static {
@@ -24,13 +25,32 @@ public class ConnectionUtil {
         dbProperties.put("user", LOGIN);
         dbProperties.put("password", PASSWORD);
         try {
-            Connection connection = DriverManager.getConnection(URL, dbProperties);
-            return connection;
+            return DriverManager.getConnection(URL, dbProperties);
         } catch (SQLException ex) {
             System.out.println("SQLException: " + ex.getMessage());
             System.out.println("SQLState: " + ex.getSQLState());
             System.out.println("VendorError: " + ex.getErrorCode());
         }
         return null;
+    }
+
+    public static void setBase(){
+        String query = FileReader.readFile("src/main/resources/init_db.sql");
+        try {
+            PreparedStatement preparedStatement = ConnectionUtil.getConnection().prepareStatement(query);
+            preparedStatement.executeUpdate();
+        } catch (SQLException throwables) {
+            throwables.printStackTrace();
+        }
+    }
+
+    public static void dropTable(){
+        String query = "drop table if exists manufacture";
+        try {
+            PreparedStatement preparedStatement = getConnection().prepareStatement(query);
+            preparedStatement.executeUpdate();
+        } catch (SQLException throwables) {
+            throwables.printStackTrace();
+        }
     }
 }
