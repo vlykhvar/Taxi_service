e8ed1bf47fc08cda20c7b69f6e83eabd29415fd6
Volodymyr Lykhvar <vlykhvar@gmail.com>
2021-01-14 14:45:22 +0200

    added DEFAULT true and changed named for column

--- src/main/resources/init_db.sql
+++ src/main/resources/init_db.sql
@@ -2,6 +2,6 @@ CREATE TABLE manufacture (
                           id INT NOT NULL AUTO_INCREMENT,
                           name VARCHAR(255),
                           country VARCHAR(100),
-                          isexist bit DEFAULT true,
+                          is_exist bit DEFAULT true,
                           PRIMARY KEY (id)
 );
