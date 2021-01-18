78eac1fe4897b043ec9577b88fa4831b1d3f654d
Volodymyr Lykhvar <vlykhvar@gmail.com>
2021-01-18 15:06:03 +0200

    added new table for an initialization

--- src/main/resources/init_db.sql
+++ src/main/resources/init_db.sql
@@ -1,7 +1,40 @@
-CREATE TABLE manufacture (
-                          id INT NOT NULL AUTO_INCREMENT,
-                          name VARCHAR(255),
-                          country VARCHAR(100),
-                          does_exist bit DEFAULT true,
-                          PRIMARY KEY (id)
-);
+CREATE TABLE `dao`.`manufactures` (
+                                      `id` BIGINT(11) NOT NULL AUTO_INCREMENT,
+                                      `name` VARCHAR(255) NULL,
+                                      `country` VARCHAR(100) NULL,
+                                      `does_exist` BIT NULL DEFAULT true,
+                                      PRIMARY KEY (`id`));
+CREATE TABLE `dao`.`drivers` (
+                                 `id` BIGINT(11) NOT NULL AUTO_INCREMENT,
+                                 `name` VARCHAR(100) NULL,
+                                 `license_number` VARCHAR(45) NULL,
+                                 `deleted` BIT NULL DEFAULT false,
+                                 PRIMARY KEY (`id`));
+
+CREATE TABLE `dao`.`cars` (
+                              `id` BIGINT(11) NOT NULL AUTO_INCREMENT,
+                              `manufacturer_id` BIGINT(11) NULL,
+                              `model` TEXT NULL,
+                              `deleted` BIT NULL DEFAULT false,
+                              PRIMARY KEY (`id`),
+                              INDEX `manufacturer_idx` (`manufacturer_id` ASC) VISIBLE,
+                              CONSTRAINT `manufacturer`
+                                  FOREIGN KEY (`manufacturer_id`)
+                                      REFERENCES `dao`.`manufactures` (`id`)
+                                      ON DELETE NO ACTION
+                                      ON UPDATE NO ACTION);
+CREATE TABLE `dao`.`cars_drivers` (
+                                      `driver_id` BIGINT(11) NULL,
+                                      `car_id` BIGINT(11) NULL,
+                                      INDEX `driver_idx` (`driver_id` ASC) VISIBLE,
+                                      INDEX `car_idx` (`car_id` ASC) VISIBLE,
+                                      CONSTRAINT `driver`
+                                          FOREIGN KEY (`driver_id`)
+                                              REFERENCES `dao`.`drivers` (`id`)
+                                              ON DELETE NO ACTION
+                                              ON UPDATE NO ACTION,
+                                      CONSTRAINT `car`
+                                          FOREIGN KEY (`car_id`)
+                                              REFERENCES `dao`.`cars` (`id`)
+                                              ON DELETE NO ACTION
+                                              ON UPDATE NO ACTION);
