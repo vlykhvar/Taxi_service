2499adece7f0950fef4035d249cf1e334aaa0257
Volodymyr Lykhvar <vlykhvar@gmail.com>
2021-01-20 10:30:32 +0200

    return wrong deleted method

--- src/main/java/mate/hwdao/dao/CarDao.java
+++ src/main/java/mate/hwdao/dao/CarDao.java
@@ -1,7 +1,9 @@
 package mate.hwdao.dao;
 
+import java.util.List;
 import mate.hwdao.model.Car;
 
 public interface CarDao extends GenericDao<Car, Long> {
 
+    List<Car> getAllByDriver(Long driverId);
 }
