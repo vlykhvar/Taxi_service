ea353b7ac3ab3f830d77b2e4b813a9dc780330aa
Volodymyr Lykhvar <vlykhvar@gmail.com>
2021-01-20 11:23:06 +0200

    added Service<T, I> interface, changed extends for service classes

--- src/main/java/mate/hwdao/service/CarService.java
+++ src/main/java/mate/hwdao/service/CarService.java
@@ -4,16 +4,7 @@ import java.util.List;
 import mate.hwdao.model.Car;
 import mate.hwdao.model.Driver;
 
-public interface CarService {
-    Car create(Car car);
-
-    Car get(Long id);
-
-    List<Car> getAll();
-
-    Car update(Car car);
-
-    boolean delete(Long id);
+public interface CarService extends Service<Car, Long> {
 
     void addDriverToCar(Driver driver, Car car);
 
