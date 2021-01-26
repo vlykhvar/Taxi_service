27593e8dcc5a608f7f17a28643fcfdcf20b9878f
Volodymyr Lykhvar <vlykhvar@gmail.com>
2021-01-26 16:58:01 +0200

    changed extend for AuthenticationException

--- src/main/java/mate/hwdao/dao/exception/AuthenticationException.java
+++ src/main/java/mate/hwdao/dao/exception/AuthenticationException.java
@@ -1,6 +1,6 @@
 package mate.hwdao.dao.exception;
 
-public class AuthenticationException extends RuntimeException {
+public class AuthenticationException extends Exception {
 
     public AuthenticationException(String message, Throwable cause) {
         super(message, cause);
